9fa1f31261f7912d8bbe52fb19c20e21a5f1218e
hashim286 <112742556+hashim286@users.noreply.github.com>
2025-03-17 17:49:50 -0400

    added a basic safety check, will add more later

--- food_database_queries/main.py
+++ food_database_queries/main.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import json
 
 mydb = mysql.connector.connect(
     host = "localhost", 
@@ -8,9 +9,10 @@ mydb = mysql.connector.connect(
 )
 
 sku = "1105917" #input("Enter a barcode to get a food: ") 
-allergen = "wheat"
+allergen = "peanuts"
 lower_allergen = allergen.lower()
 upper_allergen = allergen.upper()
+title_allergen = allergen.title()
 
 #input("Enter an allergen: ")
 
@@ -19,13 +21,26 @@ search_string = f"SELECT food.fdc_id AS SKU, branded_food.ingredients AS Ingredi
 cursor = mydb.cursor()
 
 cursor.execute(search_string)
-columns = cursor.description
-result = cursor.fetchall()
+#columns = cursor.description
+#result = cursor.fetchall()
 
-column_names = []
-for column in columns: 
-    column_names.append(column[0])
+#for thing in result: 
+#    print(thing)
 
+result = [dict((cursor.description[i][0], value) for i, value in enumerate(row)) for row in cursor.fetchall()][0]
 
-for i in range(len(column_names)): 
-    print(column_names[i] + ":   " + result[0][i])
+ingredients = result.get("Ingredients")
+
+ingredients = "MADE FROM: ENRICHED WHEAT FLOUR (FLOUR, NIACIN, REDUCED IRON, THIAMINE MONONITRATE, RIBOFLAVIN, FOLIC ACID), WATER, CONTAINS 2% OR LESS OF: SALT, CANOLA AND EXTRA VIRGIN OLIVE OILS, WHEAT GLUTEN, MALT SYRUP, YEAST, MALTED BARLEY FLOUR, RICE FLOUR, SESAME SEED MEAL.CONTAINS: WHEAT, SESAME SEED MEAT, COCONUT"
+
+if lower_allergen in ingredients or upper_allergen in ingredients: 
+    print("unsafe")
+else: 
+    print("safe")
+#column_names = []
+#for column in columns: 
+#    column_names.append(column[0])
+#
+#
+#for i in range(len(column_names)): 
+#    print(column_names[i] + ":   " + result[0][i])
